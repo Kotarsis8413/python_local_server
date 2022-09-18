739636277eee56b51ab374acba13f3f822e8e335
KoteTop8414 <93428046+KoteTop8414@users.noreply.github.com>
2022-09-18 10:50:24 +0300

    python_local_server_v3

--- START SERVER.py
+++ START SERVER.py
@@ -1,8 +1,29 @@
 f = open('port.kittm', 'r')
 fread = f.read()
-print('server started on')
-print('localhost:' + str(fread))
 from http.server import HTTPServer, CGIHTTPRequestHandler
+from tkinter import messagebox
+messagebox.showinfo('Server started', message="localhost:" + str(fread))
+print('Server Started')
 server_address = ("", int(fread))
 httpd = HTTPServer(server_address, CGIHTTPRequestHandler)
 httpd.serve_forever()
+
+
+
+
+
+
+
+
+
+#f = open('port.kittm', 'r')
+#fread = f.read()
+#print('server started on')
+#print('localhost:' + str(fread))
+#from http.server import HTTPServer, CGIHTTPRequestHandler
+#from tkinter import *
+#from tkinter import messagebox
+#server_address = ("", int(fread))
+#httpd = HTTPServer(server_address, CGIHTTPRequestHandler)
+#httpd.serve_forever()
+
