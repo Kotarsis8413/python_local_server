1430efa817ddc10ee4d7fcf001c2200ef9d41624
KoteTop8414 <93428046+KoteTop8414@users.noreply.github.com>
2022-09-18 10:56:30 +0300

    python_local_server_v3.2

--- START SERVER.py
+++ START SERVER.py
@@ -2,16 +2,23 @@ f = open('port.kittm', 'r')
 fread = f.read()
 from http.server import HTTPServer, CGIHTTPRequestHandler
 from tkinter import messagebox
-messagebox.showinfo('Server started', message="localhost:" + str(fread))
-print('Server Started')
-server_address = ("", int(fread))
-httpd = HTTPServer(server_address, CGIHTTPRequestHandler)
-httpd.serve_forever()
+if int(fread) >= 8888:
+    messagebox.showerror('Starting Error', message='Error 001')
+if int(fread) <= 8887:
+    messagebox.showinfo('Server started', message="localhost:" + str(fread))
+    print('Server Started')
+    server_address = ("", int(fread))
+    httpd = HTTPServer(server_address, CGIHTTPRequestHandler)
+    httpd.serve_forever()
 
 
 
 
-
+#messagebox.showinfo('Server started', message="localhost:" + str(fread))
+#print('Server Started')
+#server_address = ("", int(fread))
+#httpd = HTTPServer(server_address, CGIHTTPRequestHandler)
+#httpd.serve_forever()
 
 
 
